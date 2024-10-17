acc47df0aca6f9ee842292b0454ded092b1a071f
Hiniih <ogunremibioluwatife@gmail.com>
2024-10-17 12:56:57 +0100

    Add files via upload
    Library system project

---
+++ library.js
@@ -0,0 +1,20 @@
+let book = [];
+
+// funtion to add a book
+const newBook = {
+    title: "College Mathematics"
+    author: "Ho Soo Thong"
+    year: "2002"
+    genre: "Fiction"
+}
+function addABook () {
+    if newBook (title && author && year && genre) {
+        book.push(newBook)
+    }
+}
+
+// function to remove a book
+function removeABook () {
+    book.shift(newBook)
+}
+
